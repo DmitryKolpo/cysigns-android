4c6a98e20b09da357aeb46849b2532f47678366c
d.sheverin <di.sheverin@gmail.com>
2025-06-08 13:12:27 +0300

    release clean up

--- app/src/main/java/com/demacia/cysigns/features/presets/list/PresetsScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.demacia.cysigns.features.presets.list
-
--- app/src/main/java/com/demacia/cysigns/features/presets/list/ui/PresetsUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.demacia.cysigns.features.presets.list.ui
-
-data class PresetsUiState(
-    val presets: List<PresetUi>
-)
-
-data class PresetUi(
-    val title: String,
-)
